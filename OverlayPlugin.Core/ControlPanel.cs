d63af3be640d49bb6495e8b1f2823b232f29fd38
Laiglinne <mylastmogletter@gmail.com>
2016-12-21 03:39:02 +0900

    update missing files

--- OverlayPlugin.Core/ControlPanel.cs
+++ OverlayPlugin.Core/ControlPanel.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
 using System.Data;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Globalization;
-using System.Threading;
 
 namespace RainbowMage.OverlayPlugin
 {
