615f59be0186627f0018536ad933b2cf5be7d07b
Laiglinne <mylastmogletter@gmail.com>
2017-12-31 13:53:00 +0900

    Remove Unused Using headers

--- OverlayPlugin.Core/Overlays/SerializableTimerFrameEntry.cs
+++ OverlayPlugin.Core/Overlays/SerializableTimerFrameEntry.cs
@@ -1,10 +1,7 @@
 ﻿using Advanced_Combat_Tracker;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RainbowMage.OverlayPlugin.Overlays
 {
