38440635bd915d7f5d8f985bc4419956c31fa8d4
Laiglinne <mylastmogletter@gmail.com>
2018-01-01 03:18:18 +0900

    Normalize Line Ending

---
+++ Tools/LineEndingFixer/LineEndingFixer/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("LineEndingFixer")]
+[assembly: AssemblyDescription("Normalize Line Ending Tool")]
+[assembly: AssemblyProduct("LineEndingFixer")]
+[assembly: AssemblyCopyright("Copyright © Laighlinne 2018, Licensed GNU GPL-3.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("73e00963-9cfe-4ef1-b312-0e32550c06c4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
