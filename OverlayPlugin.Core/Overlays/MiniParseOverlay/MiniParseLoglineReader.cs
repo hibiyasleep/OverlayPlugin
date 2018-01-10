455758ff01ca26cdb04d39e2a25285fd704de571
Laiglinne <mylastmogletter@gmail.com>
2018-01-10 14:26:31 +0900

    Typo
    * Type for LabelOverlay
    * Detach OverlayType
    * Merge MessageType Enum
    * Fix Conflict

--- OverlayPlugin.Core/Overlays/MiniParseOverlay/MiniParseLoglineReader.cs
+++ OverlayPlugin.Core/Overlays/MiniParseOverlay/MiniParseLoglineReader.cs
@@ -5,42 +5,7 @@ using System;
 namespace RainbowMage.OverlayPlugin.Overlays
 {
     partial class MiniParseOverlay : OverlayBase<MiniParseOverlayConfig>
-    {
-        // Part of ACTWebSocket
-        // Copyright (c) 2016 ZCube; Licensed under MIT license.
-        public enum MessageType
-        {
-            LogLine = 0,
-            ChangeZone = 1,
-            ChangePrimaryPlayer = 2,
-            AddCombatant = 3,
-            RemoveCombatant = 4,
-            AddBuff = 5,
-            RemoveBuff = 6,
-            FlyingText = 7,
-            OutgoingAbility = 8,
-            IncomingAbility = 10,
-            PartyList = 11,
-            PlayerStats = 12,
-            CombatantHP = 13,
-            NetworkStartsCasting = 20,
-            NetworkAbility = 21,
-            NetworkAOEAbility = 22,
-            NetworkCancelAbility = 23,
-            NetworkDoT = 24,
-            NetworkDeath = 25,
-            NetworkBuff = 26,
-            NetworkTargetIcon = 27,
-            NetworkRaidMarker = 28,
-            NetworkTargetMarker = 29,
-            NetworkBuffRemove = 30,
-            Debug = 251,
-            PacketDump = 252,
-            Version = 253,
-            Error = 254,
-            Timer = 255
-        }
-        
+    {        
         private void LogLineReader(bool isImported, LogLineEventArgs e)
         {
             string[] d = e.logLine.Split('|');
